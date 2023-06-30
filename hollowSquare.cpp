9179e1cb6fa7b83b6c01d3db8563735f43fdfacd
Sanjit Kumar <kumarsanjit218@gmail.com>
2023-06-30 14:55:09 +0530

    Initial commit

---
+++ hollowSquare.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        for (int j = 0; j < n; j++)
+        {
+            cout << " ";
+        }
+        for (int i = 0;i < n;i++){
+            if(i==0|| i==n-1){
+                cout << "*";
+            }
+        }cout << endl;
+    }
+}
